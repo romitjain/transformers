9fbd433d3be2dc065944ebab6261e62f76ade753
romit <11757603+romitjain@users.noreply.github.com>
2024-07-22 12:43:42 +0530

    Updated the calculation of num frames in case there is a remainder

--- src/transformers/models/seamless_m4t/feature_extraction_seamless_m4t.py
+++ src/transformers/models/seamless_m4t/feature_extraction_seamless_m4t.py
@@ -286,8 +286,10 @@ class SeamlessM4TFeatureExtractor(SequenceFeatureExtractor):
 
         remainder = num_frames % self.stride
         if remainder != 0:
-            input_features = input_features[:, :num_frames, :]
-            attention_mask = attention_mask[:, :num_frames]
+            input_features = input_features[:, :num_frames-remainder, :]
+            attention_mask = attention_mask[:, :num_frames-remainder]
+
+            num_frames = num_frames-remainder
 
         input_features = np.reshape(
             input_features, (batch_size, num_frames // self.stride, num_channels * self.stride)
